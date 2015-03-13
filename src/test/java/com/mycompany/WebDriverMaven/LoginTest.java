698bcfd9211e82e235709afb477aeb44b819e53d
SandeepGangireddy <sandeepg8081@gmail.com>
2015-03-13 23:24:31 +0000

    Initial Commit

---
+++ src/test/java/com/mycompany/WebDriverMaven/LoginTest.java
@@ -0,0 +1,41 @@
+package com.mycompany.WebDriverMaven;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+
+public class LoginTest {
+
+
+	
+		public static WebDriver driver;
+		
+		@BeforeSuite
+		public void setup() {
+		
+			driver = new FirefoxDriver();
+		}
+		
+		@Test
+		public void doLogin() {
+			
+			driver.get("https://gmail.com");
+			driver.findElement(By.id("Email")).sendKeys("sandeepg8081@gmail.com");
+			driver.findElement(By.id("Passwd")).sendKeys("sdfsdf");
+			
+		}
+
+		
+		@AfterSuite
+		public void tearDown() {
+			
+			driver.quit();
+		}
+	}
+
+
