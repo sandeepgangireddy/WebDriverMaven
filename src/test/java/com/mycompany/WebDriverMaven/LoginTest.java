b5e3650a4079da66240a1f894e0ead08b17152fd
SandeepGangireddy <sandeepg8081@gmail.com>
2015-03-14 02:08:34 +0000

    more changes

--- src/test/java/com/mycompany/WebDriverMaven/LoginTest.java
+++ src/test/java/com/mycompany/WebDriverMaven/LoginTest.java
@@ -29,7 +29,7 @@ public class LoginTest {
 			driver.findElement(By.id("Passwd")).sendKeys("sdfsdf");
 			driver.findElement(By.id("SignIn")).click();
 			
-			//added new comments
+			//added new comments - more
 			
 		}
 
