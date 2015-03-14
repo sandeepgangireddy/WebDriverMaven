6fdf2a1b1144c48e12f678f15535cd860f1dd759
SandeepGangireddy <sandeepg8081@gmail.com>
2015-03-14 12:21:11 +0000

    TAKE4

--- src/test/java/com/mycompany/WebDriverMaven/LoginTest.java
+++ src/test/java/com/mycompany/WebDriverMaven/LoginTest.java
@@ -29,7 +29,7 @@ public class LoginTest {
 			driver.findElement(By.id("Passwd")).sendKeys("sdfsdf");
 			driver.findElement(By.id("signIn")).click();
 			
-			//added new comments - To Make stable 
+			//added new comments - To Make stable nnn
 			
 		}
 
